64a0d90f50b4038b35922d2b4e98585bb744c14c
Faizal Nugraha <faizaldwinugraha@gmail.com>
2024-09-15 02:11:51 +0700

    role users relation attach detach detachAny

--- app/Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -2,17 +2,35 @@
 
 namespace App\Filament\Resources\RoleResource\RelationManagers;
 
+use App\Models\Role;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Gate;
 
 class UsersRelationManager extends RelationManager
 {
     protected static string $relationship = 'users';
+
+    protected function canAttach(): bool
+    {
+        return Gate::check('attach', Role::class);
+    }
+
+    protected function canDetach(Model $record): bool
+    {
+        return Gate::check('detach', $this->getOwnerRecord());
+    }
+
+    protected function canDetachAny(): bool
+    {
+        return Gate::check('detachAny', Role::class);
+    }
 
     public function table(Table $table): Table
     {
