fc4c9eef297c297f3dd121a654e6aec43ded962f
Faizal Nugraha <faizaldwinugraha@gmail.com>
2024-09-15 16:38:56 +0700

    user delete test

---
+++ tests/Feature/UserDeleteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Enums\Permission;
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+use Filament\Actions\DeleteAction;
+
+use function Pest\Livewire\livewire;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+
+    givePermissions($this->user, [
+        Permission::USERS_VIEWANY,
+        Permission::USERS_VIEW,
+        Permission::USERS_DELETE,
+    ]);
+
+    $this->actingAs($this->user);
+});
+
+it('can delete', function () {
+    $user = User::factory()->create();
+
+    livewire(UserResource\Pages\ViewUser::class, ['record' => $user->getRouteKey()])
+        ->callAction(DeleteAction::class);
+
+    $this->assertDatabaseMissing('users', ['id' => $user->id]);
+});
