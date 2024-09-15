f44d6ca548444be73a03290d0604ecd2262e5df8
Faizal Nugraha <faizaldwinugraha@gmail.com>
2024-09-15 16:22:25 +0700

    user create test

---
+++ tests/Feature/UserCreateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Enums\Permission;
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+
+    givePermissions($this->user, [
+        Permission::USERS_VIEWANY,
+        Permission::USERS_CREATE,
+    ]);
+
+    $this->actingAs($this->user);
+});
+
+it('can render page', function () {
+    $this
+        ->get(UserResource::getUrl('create'))
+        ->assertSuccessful();
+});
