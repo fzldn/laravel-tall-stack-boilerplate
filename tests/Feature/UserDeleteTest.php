ff3bb6c34f89f9078b85c69d4ce10f63bd65fddd
Faizal Nugraha <faizaldwinugraha@gmail.com>
2024-09-15 22:30:06 +0700

    test: user able to delete

--- tests/Feature/UserDeleteTest.php
+++ tests/Feature/UserDeleteTest.php
@@ -19,6 +19,13 @@ beforeEach(function () {
     $this->actingAs($this->user);
 });
 
+it('able to delete', function () {
+    $user = User::factory()->create();
+
+    livewire(UserResource\Pages\ListUsers::class)
+        ->assertTableActionVisible(DeleteAction::class, record: $user);
+});
+
 it('can delete', function () {
     $user = User::factory()->create();
 
@@ -28,9 +35,23 @@ it('can delete', function () {
     $this->assertDatabaseMissing(User::class, ['id' => $user->id]);
 });
 
+it('not able to delete self', function () {
+    livewire(UserResource\Pages\ListUsers::class)
+        ->assertTableActionHidden(DeleteAction::class, record: $this->user);
+});
+
 it('cannot delete self', function () {
     livewire(UserResource\Pages\ViewUser::class, ['record' => $this->user->getRouteKey()])
         ->assertActionHidden(DeleteAction::class);
+});
+
+it('not able to delete super admin', function () {
+    $user = User::factory()->create();
+
+    assignSuperAdminRole($user);
+
+    livewire(UserResource\Pages\ListUsers::class)
+        ->assertTableActionHidden(DeleteAction::class, record: $user);
 });
 
 it('cannot delete super admin', function () {
