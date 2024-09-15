8851f5872e087813f3a855d4ef7ae5eae7dafcc4
Faizal Nugraha <faizaldwinugraha@gmail.com>
2024-09-15 16:16:43 +0700

    user view test

---
+++ tests/Feature/UserViewTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Enums\Permission;
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+
+    createAndAssignRole($this->user, [
+        Permission::USERS_VIEWANY,
+        Permission::USERS_VIEW,
+        Permission::ROLES_VIEWANY,
+    ]);
+
+    $this->actingAs($this->user);
+});
+
+it('can render page', function () {
+    $user = User::factory()->create();
+
+    $this
+        ->get(UserResource::getUrl('view', ['record' => $user]))
+        ->assertSuccessful();
+});
